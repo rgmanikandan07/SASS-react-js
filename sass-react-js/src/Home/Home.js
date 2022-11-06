c81ea1ea41fa0866bd51725e104f34139e9b1f20
rgmanikandan07 <rgmanimani0707@gmail.com>
2022-11-06 11:34:17 +0530

    add new file

---
+++ sass-react-js/src/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.scss'
+
+const Home = () => {
+  return (
+    <div className='container'>
+      <h3>Name : Dhoni</h3>
+      <h5>Role : WK/Batsman</h5>
+      <p>Finisher</p>
+      <button>Records</button>
+    </div>
+  )
+}
+
+export default Home
